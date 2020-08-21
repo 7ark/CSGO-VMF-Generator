3ef5f8ad3be86be2248165044fd930fd081d5878
Cory Koseck <koseckcory@gmail.com>
2020-08-21 11:30:00 -0500

    Updated result path

--- VMFGeneration/Program.cs
+++ VMFGeneration/Program.cs
@@ -6,7 +6,15 @@ namespace VMFConverter
     {
         static void Main(string[] args)
         {
-            string vmfPath = @"C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\sdk_content\maps\experiment3.vmf";
+            string directory = @"C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\sdk_content\maps";
+            if(args.Length > 0)
+            {
+                if(Directory.Exists(args[0]))
+                {
+                    directory = args[0];
+                }
+            }
+            string vmfPath = directory + @"\GeneratedMap.vmf";
 
             Generator gen = new Generator();
             File.WriteAllText(vmfPath, gen.Generate(vmfPath));
